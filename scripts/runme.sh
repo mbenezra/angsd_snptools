b105b7d7793b4ffaa17bd59131e2b03c44909763
lxc844 <lxc844@a00643.science.domain>
2017-12-26 21:41:13 +0100

    added

---
+++ scripts/runme.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sh list_bams.sh
+
+sh download_hg19.sh
+
+sh get_hapmap.sh
+
+sh run_snptools_pileup.sh
+
+sh run_snptools_varisite.sh
+
+sh run_snptools_bamodel.sh
+
+sh run_snptools_poprob.sh
+
+sh run_snptools_prob2vcf.sh
