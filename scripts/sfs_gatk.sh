68477952c224f1c88c6949bdb91e17cb13ca505e
lxc844 <lxc844@a00693.science.domain>
2017-12-30 22:05:52 +0100

    updated

--- scripts/sfs_gatk.sh
+++ scripts/sfs_gatk.sh
@@ -4,6 +4,6 @@ mkdir sfs_gatk
 
 less -S bams.list | head -n 10 > sfs_gatk/CEU.filelist
 
-angsd/angsd -sites prob2vcf/sites.txt -b sfs_gatk/CEU.filelist -anc hg19anc/chimpHg19.fa.gz -out sfs_gatk/gatk.ceu -dosaf 1 -gl 1 -ref hg19/hg19.fa.gz -domajorminor 3
+angsd/angsd -sites prob2vcf/sites.txt -b sfs_gatk/CEU.filelist -anc hg19anc/chimpHg19.fa.gz -out sfs_gatk/gatk.ceu -doSaf 5 -GL 2 -ref hg19/hg19.fa.gz -doMajorMinor 1
 
-realSFS sfs_gatk/gatk.ceu.saf.idx > sfs_gatk/gatk.ceu.sfs
+angsd/misc/realSFS sfs_gatk/gatk.ceu.saf.idx > sfs_gatk/gatk.ceu.sfs
--- scripts/sfs_snptools.sh
+++ scripts/sfs_snptools.sh
@@ -4,6 +4,8 @@ mkdir sfs_snptools
 
 less -S bams.list | head -n 10 > sfs_snptools/CEU.filelist
 
-angsd/angsd -sites prob2vcf/sites.txt -b sfs_snptools/CEU.filelist -anc hg19anc/chimpHg19.fa.gz -out sfs_snptools/snptools.ceu -dosaf 1 -gl 1 -ref hg19/hg19.fa.gz -domajorminor 3
+angsd/angsd -vcf-GL prob2vcf/10indv.out.recode.vcf.zip -doSaf 5 -fai hg19/hg19.fa.gz.fai -anc hg19anc/chimpHg19.fa.gz -nind 10 -doMajorMinor 1 -out sfs_snptools/snptools.ceu
 
-realSFS sfs_snptools/snptools.ceu.saf.idx > sfs_snptools/snptools.ceu.sfs
+# angsd/angsd -sites prob2vcf/sites.txt -b sfs_snptools/CEU.filelist -anc hg19anc/chimpHg19.fa.gz -out sfs_snptools/snptools.ceu -dosaf 1 -gl 1 -ref hg19/hg19.fa.gz -domajorminor 3
+
+angsd/misc/realSFS sfs_snptools/snptools.ceu.saf.idx > sfs_snptools/snptools.ceu.sfs
