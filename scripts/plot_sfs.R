40cb342801759b1b7b6995d2d3c852fab2dbb9c1
lxc844 <lxc844@a00643.science.domain>
2017-12-31 13:09:56 +0100

    added

--- scripts/plot_sfs.R
+++ scripts/plot_sfs.R
@@ -3,9 +3,9 @@
 nnorm <- function(x) x/sum(x)
 #expected number of sites with 1:20 derived alleles
 res <- rbind(
-  GATK=scan("../sfs/gatk.ceu.sfs")[-1],
-  ANGSD=scan("../sfs/angsd.ceu.sfs")[-1],
-  SNPTOOLS=scan("../sfs/snptools.ceu.sfs")[-1]
+  GATK=scan("sfs_gatk/gatk.ceu.sfs")[-1],
+  SAMTOOLS=scan("sfs_sam/sam.ceu.sfs")[-1],
+  SNPTOOLS=scan("sfs_snptools/snptools.ceu.sfs")[-1]
 )
 colnames(res) <- 1:20
 
@@ -13,11 +13,11 @@ colnames(res) <- 1:20
 res <- t(apply(res,1,nnorm))
 
 #plot the none ancestral sites
-barplot(res,beside=T,legend=c("GATK","ANGSD","SNPTOOLS"),names=1:20,main="realSFS non ancestral sites")
+barplot(res,beside=T,legend=c("GATK","SAMTOOLS","SNPTOOLS"),names=1:20,main="realSFS non ancestral sites")
 
 #plot the polymorphic sites.
 resPoly <- t(apply(res[,-20],1,nnorm))
-barplot(resPoly,beside=T,legend=c("GATK","ANGSD","SNPTOOLS"),names=1:19,main="realSFS polymorphic sites")
+barplot(resPoly,beside=T,legend=c("GATK","SAMTOOLS","SNPTOOLS"),names=1:19,main="realSFS polymorphic sites")
 
 #due the very limited amount of data lets
 #downsample to 5 individuals (10 chromosome) and exclude fixed derived
@@ -27,6 +27,6 @@ downsampleSFS <- function(x,chr){ #x 1:2n , chr < 2n
     nnorm( as.vector(t(mat) %*% x)[-chr] )
 }
 resDown <- t(apply(res,1,downsampleSFS,chr=10))
-barplot(resDown,beside=T,legend=c("GATK","ANGSD","SNPTOOLS"),names=1:9,main="realSFS downsampled polymorphic sites")
+barplot(resDown,beside=T,legend=c("GATK","SAMTOOLS","SNPTOOLS"),names=1:9,main="realSFS downsampled polymorphic sites")
 
 
