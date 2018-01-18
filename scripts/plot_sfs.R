d33261561c73b48b3ca1caab922cc6c87b26771b
Michael Benezra <mbenezra@Michaels-MacBook-Air.local>
2018-01-18 10:22:22 +0100

    updated

--- scripts/plot_sfs.R
+++ scripts/plot_sfs.R
@@ -7,21 +7,30 @@ res <- rbind(
   SAMTOOLS=scan("sfs_sam/sam.ceu.sfs")[-1],
   SNPTOOLS=scan("sfs_snptools/snptools.ceu.sfs")[-1]
 )
+
+res2 <- rbind(
+  GATK=scan("sfs_gatk/gatk.ceu.sfs"),
+  SAMTOOLS=scan("sfs_sam/sam.ceu.sfs"),
+  SNPTOOLS=scan("sfs_snptools/snptools.ceu.sfs")
+)
+
 colnames(res) <- 1:10
 
 # density instead of expected counts
 res <- t(apply(res,1,nnorm))
 
 #plot the none ancestral sites
-png('realSFS_non_ancestral_sites.png',width=1024,height=1024)
-barplot(res,beside=T,legend=c("GATK","SAMTOOLS","SNPTOOLS"),names=1:10,main="realSFS non ancestral sites")
+png('sfs.png',width=1024,height=1024)
+barplot(res,beside=T,legend=c("GATK: 0.6100569 variability","SAMTOOLS: 0.6024786 variability","SNPTools: 0.6030279 variability"),names=1:10,main="Folded Site Frequency Spectrum (SFS)")
 dev.off()
 
+1-res2[,1]/rowSums(res2)
+
 #plot the polymorphic sites.
-resPoly <- t(apply(res[,-10],1,nnorm))
-png('realSFS_ploymorphic_sites.png',width=1024,height=1024)
-barplot(resPoly,beside=T,legend=c("GATK","SAMTOOLS","SNPTOOLS"),names=1:9,main="realSFS polymorphic sites")
-dev.off()
+# resPoly <- t(apply(res[,-10],1,nnorm))
+# png('realSFS_ploymorphic_sites.png',width=1024,height=1024)
+# barplot(resPoly,beside=T,legend=c("GATK","SAMTOOLS","SNPTOOLS"),names=1:9,main="realSFS polymorphic sites")
+# dev.off()
 
 #due the very limited amount of data lets
 #downsample to 5 individuals (10 chromosome) and exclude fixed derived
